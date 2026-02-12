675534c932f190946b23e3fd76a942d03765b381
Pruthviraj Rathod <125789327+pruthvi4344@users.noreply.github.com>
2026-02-12 14:21:08 -0500

    chages in about function in views.py

--- app/views.py
+++ app/views.py
@@ -54,7 +54,7 @@ def pricing(request):
                                            'pricingmanagedetails':pricingmanagedetails,
                                            'plans':plans
                                            })
-
+//this call the about page
 def about(request):
     corevaluedetails = Corevalue.objects.all()
     contactdetails = Contact.objects.first()
