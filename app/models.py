ecdcabf2ba130bb9feeafc56a3116aac66748ca0
Pruthviraj Rathod <125789327+pruthvi4344@users.noreply.github.com>
2026-03-30 11:22:34 -0400

    User model to app/models.py
    Added a User model with username and password fields.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# user model added
 class User(models.Model):
    username = models.EmailField(max_length=100)
    password = models.CharField(max_length=50)
